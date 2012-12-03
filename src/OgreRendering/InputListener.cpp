3d96041b43dd30901bc7d241b71caca9f67be1fb
Anis <Benyoub.anis@gmail.com>
2012-12-03 16:41:52 +0100

    Default key press

--- src/OgreRendering/InputListener.cpp
+++ src/OgreRendering/InputListener.cpp
@@ -34,7 +34,7 @@
 // class InputListener
 ///////////////////////////////////////////////////////////////////////////////
 
-  template<> InputListener* Singleton<InputListener>::mySingleton = 0;
+template<> InputListener* Singleton<InputListener>::mySingleton = 0;
 
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -46,57 +46,57 @@
  * Constructor.
  */
 InputListener::InputListener ( Ogre::RenderWindow * aWindow, Renderer * aViewer, Ogre::Camera * aCamera, Ogre::SceneManager * aSceneManager ) :
-  myWindow ( aWindow ), myViewer ( aViewer )
+    myWindow ( aWindow ), myViewer ( aViewer )
 
 {
 
-  // Camera Intiate
-  myCamera = aCamera;
-  Ogre::SceneNode * aNode=aSceneManager->getRootSceneNode()->createChildSceneNode();
-   myCameraMan = new CameraMan (aNode, myCamera );
+    // Camera Intiate
+    myCamera = aCamera;
+    Ogre::SceneNode * aNode=aSceneManager->getRootSceneNode()->createChildSceneNode();
+    myCameraMan = new CameraMan (aNode, myCamera );
 
-  size_t windowHnd = 0;
-  std::ostringstream windowHndStr;
+    size_t windowHnd = 0;
+    std::ostringstream windowHndStr;
     
-  myWindow->getCustomAttribute("WINDOW", &windowHnd);
-  windowHndStr << windowHnd;
+    myWindow->getCustomAttribute("WINDOW", &windowHnd);
+    windowHndStr << windowHnd;
 
-  OIS::ParamList pl;
-  pl.insert(std::make_pair(std::string("WINDOW"), windowHndStr.str()));
-  myInputManager = OIS::InputManager::createInputSystem( pl );
+    OIS::ParamList pl;
+    pl.insert(std::make_pair(std::string("WINDOW"), windowHndStr.str()));
+    myInputManager = OIS::InputManager::createInputSystem( pl );
 
-  //Create all devices (We only catch joystick exceptions here, as, most people have Key/Mouse)
-  myKeyboard = static_cast<OIS::Keyboard*>(myInputManager->createInputObject( OIS::OISKeyboard, true ));
-  myMouse = static_cast<OIS::Mouse*>(myInputManager->createInputObject( OIS::OISMouse, true ));
+    //Create all devices (We only catch joystick exceptions here, as, most people have Key/Mouse)
+    myKeyboard = static_cast<OIS::Keyboard*>(myInputManager->createInputObject( OIS::OISKeyboard, true ));
+    myMouse = static_cast<OIS::Mouse*>(myInputManager->createInputObject( OIS::OISMouse, true ));
 
-  myMouse->setEventCallback(this);
-  myKeyboard->setEventCallback(this);
-  // End flag initate
-  myShutdown = false;
+    myMouse->setEventCallback(this);
+    myKeyboard->setEventCallback(this);
+    // End flag initate
+    myShutdown = false;
 
 
     
-  const OIS::MouseState &ms = myMouse->getMouseState();
-  ms.width = myWindow->getWidth();
-  ms.height = myWindow->getHeight();
+    const OIS::MouseState &ms = myMouse->getMouseState();
+    ms.width = myWindow->getWidth();
+    ms.height = myWindow->getHeight();
  
 
 
-  // Other initating
-  myIsInShiftMode = false;
-  mySceneMgr = aSceneManager;
+    // Other initating
+    myIsInShiftMode = false;
+    mySceneMgr = aSceneManager;
 
-  //Register as a Window listener
-  Ogre::WindowEventUtilities::addWindowEventListener(myWindow, this);
+    //Register as a Window listener
+    Ogre::WindowEventUtilities::addWindowEventListener(myWindow, this);
 
 
-  // Initiating timer
-  myTimer = OGRE_NEW Ogre::Timer();
-  myTimer->reset();
+    // Initiating timer
+    myTimer = OGRE_NEW Ogre::Timer();
+    myTimer->reset();
 
-  myWindow->setActive(true);
-  myTime =-1;
-  myWindow->setDeactivateOnFocusChange(false);
+    myWindow->setActive(true);
+    myTime =-1;
+    myWindow->setDeactivateOnFocusChange(false);
 
 
 
@@ -109,90 +109,92 @@ InputListener::InputListener ( Ogre::RenderWindow * aWindow, Renderer * aViewer,
  */
 InputListener::~InputListener()
 {
-  myInputManager->destroyInputObject ( myKeyboard );
-  myInputManager->destroyInputObject ( myMouse );
-  delete myCameraMan;
-  OIS::InputManager::destroyInputSystem ( myInputManager );
+    myInputManager->destroyInputObject ( myKeyboard );
+    myInputManager->destroyInputObject ( myMouse );
+    delete myCameraMan;
+    OIS::InputManager::destroyInputSystem ( myInputManager );
 }
 
 
 
 bool InputListener::frameRenderingQueued(const Ogre::FrameEvent & evt)
-		{
+{
 
-			if(myMouse)
-				myMouse->capture();
-			if(myKeyboard)
-				myKeyboard->capture();
-			if (myWindow->isClosed() || myShutdown)
-					return false;
+    if(myMouse)
+	myMouse->capture();
+    if(myKeyboard)
+	myKeyboard->capture();
+    if (myWindow->isClosed() || myShutdown)
+	return false;
 
-			myViewer->Update(evt.timeSinceLastFrame);
-			myCameraMan->render(evt.timeSinceLastFrame);
-			return true;
-		}
+    myViewer->Update(evt.timeSinceLastFrame);
+    myCameraMan->render(evt.timeSinceLastFrame);
+    return true;
+}
 
 bool InputListener::frameStarted(const Ogre::FrameEvent & evt)
 {
-	return true;
+    return true;
 }
 
 bool InputListener::keyPressed(const OIS::KeyEvent & evt)
 {
-	switch (evt.key)
-	{
+    switch (evt.key)
+    {
 
-		case OIS::KC_ESCAPE:
-			myShutdown = true;
-			return false;
-			break;
-		case OIS::KC_SPACE:
-			return true;
-				break;
-	}
-	myCameraMan->handleKeyPress(evt);
+    case OIS::KC_ESCAPE:
+	myShutdown = true;
+	return false;
+	break;
+    case OIS::KC_SPACE:
 	return true;
+	break;
+    default:
+	break;
+    }
+    myCameraMan->handleKeyPress(evt);
+    return true;
 
 }
 
 
 bool InputListener::frameEnded (const Ogre::FrameEvent &evt)
 {
-	return true;
+    return true;
 }
 
 
 
 bool InputListener::mousePressed(const OIS::MouseEvent &arg, OIS::MouseButtonID id)
 {
-	myCameraMan->handleMouseClick(arg,id);
-	return true;
+    myCameraMan->handleMouseClick(arg,id);
+    return true;
 
 }
 
 bool InputListener::mouseReleased(const OIS::MouseEvent &e, OIS::MouseButtonID id)
 {
-	myCameraMan->handleMouseRelease(e,id);
-	return true;
+    myCameraMan->handleMouseRelease(e,id);
+    return true;
 }
 
 bool InputListener::keyReleased(const OIS::KeyEvent &e)
 {
-	myCameraMan->handleKeyRelease(e);
-	return true;
+    myCameraMan->handleKeyRelease(e);
+    return true;
 }
 
 
 bool InputListener::mouseMoved(const OIS::MouseEvent &e)
 {
-	myCameraMan->handleMouseMove(e);
-	return true;
+    myCameraMan->handleMouseMove(e);
+    return true;
 }/**
- * makes the render loop stop
- */
+  * makes the render loop stop
+  */
 void InputListener::stopRendering ( )
 {
-  myShutdown = true;
+    myShutdown = true;
 }
 
 
@@ -203,14 +205,14 @@ void InputListener::stopRendering ( )
  */
 void InputListener::startRendering ( )
 {
-  myShutdown = false;
+    myShutdown = false;
 }
 
 
 /**
  * Handles a window resize
  */
- void InputListener::windowResized(Ogre::RenderWindow* rw)
+void InputListener::windowResized(Ogre::RenderWindow* rw)
 {
 
 }
@@ -219,6 +221,6 @@ void InputListener::startRendering ( )
  */
 void InputListener::windowClosed(Ogre::RenderWindow* rw)
 {
-  myShutdown=true;
+    myShutdown=true;
 
 }
